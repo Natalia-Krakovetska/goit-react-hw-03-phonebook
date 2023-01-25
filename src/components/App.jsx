0ce6e94ebf3a08deefef26bb1bd0c55204b9b87b
Natalia <natalakrakoveckaya@gmail.com>
2023-01-25 18:55:59 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,19 @@ export class App extends Component {
     contacts: [],
     filter: '',
   };
+componentDidMount(){
+const contacts = localStorage.getItem('contacts');
+const parcedContacts = JSON.parse(contacts);
+if (parcedContacts){
+  this.setState ({ contacts: parcedContacts })
+};
+};
 
+componentDidUpdate(prevProps, prevState){
+if (this.state.contacts !== prevState.contacts){
+localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+}
+}
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
     const alreadyInContacts = contacts.find(
@@ -46,6 +58,7 @@ export class App extends Component {
       contacts: prevstate.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
 
   render() {
     const { filter } = this.state;
